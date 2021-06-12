c1f4555cf28ad267c80993a30a92dad6dcfcbe42
Luis <Urenaperdomo@gmail>
2021-06-12 17:27:54 -0400

    Ejercicios de SQLite

---
+++ Interfaz grafica SQLite/idleArticulos.py
@@ -0,0 +1,71 @@
+import sqlite3
+from tkinter.constants import W
+
+class Articulos:
+
+    def abrir(self):
+        conexion=sqlite3.connect("bd1.db")
+        return conexion
+
+
+    def alta(self, datos):
+        cone=self.abrir()
+        cursor=cone.cursor()
+        sql="insert into articulos(descripcion, precio) values (?,?)"
+        cursor.execute(sql, datos)
+        cone.commit()
+        cone.close()
+
+    def consulta(self, datos):
+        try:
+            cone=self.abrir()
+            cursor=cone.cursor()
+            sql="select descripcion, precio from articulos where codigo=?"
+            cursor.execute(sql, datos)
+            return cursor.fetchall()
+        finally:
+            cone.close()
+            
+    def recuperar_todos(self):
+        try:
+            cone=self.abrir()
+            cursor=cone.cursor()
+            sql="select codigo, descripcion, precio from articulos"
+            cursor.execute(sql)
+            return cursor.fetchall()
+        finally:
+            cone.close()
+    
+    def baja(self, datos):
+        try:
+            cone=self.abrir()
+            cursor=cone.cursor()
+            sql="delete from articulos where codigo=?"
+            cursor.execute(sql, datos)
+            return cursor.rowcount
+        finally:
+            cone.commit()
+            cone.close()
+    
+    def consulta_mod(self, datos):
+        try:
+            print("desde la base de datos , el dato es ", datos)
+            cone=self.abrir()
+            cursor=cone.cursor()
+            sql="select descripcion, precio from articulos where codigo=?"
+            cursor.execute(sql, datos)
+            return cursor.fetchall()
+        finally:
+            cone.close()
+    
+    def modificacion(self, datos):
+        try:
+            print("desde la base de datos la actualizacion es:", datos)
+            cone=self.abrir()
+            cursor=cone.cursor()
+            sql="update articulos set descripcion=?, precio=? where codigo=?"
+            cursor.execute(sql, datos)
+            return cursor.rowcount
+        finally:
+            cone.commit()
+            cone.close()
--- Interfaz grafica SQLite/idleFormularios.py
+++ Interfaz grafica SQLite/idleFormularios.py
@@ -1,14 +1,12 @@
-# Agregar dos pestañas al programa de administración de artículos que permitan borrar un artículo ingresando su código y otra opción 
-# que permita consultar y modificar la descripción y precio de un artículo.
-
 import tkinter as tk
-from tkinter import ttk
+from tkinter import StringVar, ttk
 from tkinter import messagebox as mb
 from tkinter import scrolledtext as st
-import articulos
+import idleArticulos
+
 class FormularioArticulos:
     def __init__(self):
-        self.articulo1=articulos.Articulos()
+        self.articulo1=idleArticulos.Articulos()
         self.ventana1=tk.Tk()
         self.ventana1.title("Mantenimiento de artículos")
         self.cuaderno1 = ttk.Notebook(self.ventana1)        
@@ -16,7 +14,7 @@ class FormularioArticulos:
         self.consulta_por_codigo()
         self.listado_completo()
         self.borrar_articulo()
-        self.modificar_consultar()
+        self.modificacion()
         self.cuaderno1.grid(column=0, row=0, padx=10, pady=10)
         self.ventana1.mainloop()
 
@@ -67,7 +65,7 @@ class FormularioArticulos:
         self.entryprecio.grid(column=1, row=2, padx=4, pady=4)
         self.boton1=ttk.Button(self.labelframe2, text="Consultar", command=self.consultar)
         self.boton1.grid(column=1, row=3, padx=4, pady=4)
-    
+
     def consultar(self):
         datos=(self.codigo.get(), )
         respuesta=self.articulo1.consulta(datos)
@@ -75,9 +73,9 @@ class FormularioArticulos:
             self.descripcion.set(respuesta[0][0])
             self.precio.set(respuesta[0][1])
         else:
-            mb.showinfo("Informacion", "No existe ningun articulo con dicho codigo de identificacion")
-
-        print(datos)
+            self.descripcion.set('')
+            self.precio.set('')
+            mb.showinfo("Información", "No existe un artículo con dicho código")
 
     def listado_completo(self):
         self.pagina3 = ttk.Frame(self.cuaderno1)
@@ -95,75 +93,74 @@ class FormularioArticulos:
         for fila in respuesta:
             self.scrolledtext1.insert(tk.END, "código:"+str(fila[0])+"\ndescripción:"+fila[1]+"\nprecio:"+str(fila[2])+"\n\n")
 
-
     def borrar_articulo(self):
         self.pagina4=ttk.Frame(self.cuaderno1)
         self.cuaderno1.add(self.pagina4, text="Borrar articulo")
-        self.labelframe4=ttk.LabelFrame(self.pagina4, text="Articulo")
-        self.labelframe4.grid(column=0, row=0, padx=5, pady=10)
-        self.label4=ttk.Label(self.labelframe4, text="Codigo:")
+        self.labelframe4=ttk.Labelframe(self.pagina4, text="articulo")
+        self.labelframe4.grid(column=0, row=0, padx=5, pady=5)
+        self.label4=ttk.Label(self.labelframe4, text="Codigo del articulo:")
         self.label4.grid(column=0, row=0, padx=4, pady=4)
-        self.dato=tk.StringVar()
-        self.codigoentry=ttk.Entry(self.labelframe4, textvariable=self.dato)
-        self.codigoentry.grid(column=1, row=0)
-        self.boton=ttk.Button(self.labelframe4, text="Borrar", command=self.borrar_dato)
-        self.boton.grid(column=1, row=2, pady=4)
+        self.dato=StringVar()
+        self.entrydato=ttk.Entry(self.labelframe4, textvariable=self.dato)
+        self.entrydato.grid(column=1, row=0, padx=4, pady=4)
+        self.boton1=ttk.Button(self.labelframe4, text="Eliminar", command=self.eliminar)
+        self.boton1.grid(column=1, row=1, padx=4, pady=5)
+    
+    def eliminar(self):
+        codigo=(self.dato.get(), )
+        respuesta=self.articulo1.baja(codigo)
 
-    def borrar_dato(self):
-        codigo= (self.dato.get(), )
-        cantidad=self.articulo1.eliminar(codigo)
-        if cantidad==1:
-            mb.showinfo("Informacion", "el dato se ha eliminado exitosamente")
+        if respuesta > 0:
+            mb.showinfo("Informacion!", "El articulo ha sido eliminado")
         else:
-            mb.showerror("Error!","No existe ningun articulo con dicho codigo")
-
-    def modificar_consultar(self):
-        self.pagina5=ttk.Frame(self.cuaderno1)
-        self.cuaderno1.add(self.pagina5, text="Modificar articulo")
-        self.labelframe5=ttk.LabelFrame(self.pagina5, text="Articulo")
-        self.labelframe5.grid(column=0, row=0, padx=5, pady=10)
-        self.label1=ttk.Label(self.labelframe5, text="Codigo:")
-        self.label1.grid(column=0, row=0, padx=4, pady=4)
-        self.codemod=tk.StringVar()
-        self.codentry=ttk.Entry(self.labelframe5, textvariable=self.codemod)
-        self.codentry.grid(column=1, row=0)
-        self.label2=ttk.Label(self.labelframe5, text="Descripcion:")
-        self.label2.grid(column=0, row=1, padx=4, pady=4)
-        self.descriptionmod=tk.StringVar()
-        self.descripentry=ttk.Entry(self.labelframe5, textvariable=self.descriptionmod)
-        self.descripentry.grid(column=1, row=1)
-        self.label3=ttk.Label(self.labelframe5, text="Precio:")
-        self.label3.grid(column=0, row=2, padx=4, pady=4)
-        self.pricemod=tk.StringVar()
-        self.pricentry=ttk.Entry(self.labelframe5, textvariable=self.pricemod)
-        self.pricentry.grid(column=1, row=2)
-        self.boton1=ttk.Button(self.labelframe5, text="Consultar", command=self.consultarmod)
-        self.boton1.grid(column=1, row=3)
-        self.boton2=ttk.Button(self.labelframe5, text="Modificar", command=self.modificacion)
-        self.boton2.grid(column=1, row=4)
-
-    def consultarmod(self):
-        datos=(self.codemod.get(), )
-        respuesta=self.articulo1.consulta(datos)
-        if len(respuesta)>0:
-            self.descriptionmod.set(respuesta[0][0])
-            self.pricemod.set(respuesta[0][1])
-        else:
-            self.descriptionmod.set('')
-            self.pricemod.set('')
-            mb.showinfo("Información", "No existe un artículo con dicho código")
-        
+            mb.showerror("Error!", "No existe ningun articulo con ese codigo")
+    
     def modificacion(self):
-        if self.descriptionmod.get()=="" or self.pricemod.get()=="" or self.codemod.get()=="":
-            mb.showerror("Error!", "falto rellenar algunos de los campos")
-        else:
-            datos=(self.descriptionmod.get(), self.pricemod.get(), self.codemod.get())
-            cantidad=self.articulo1.cambiar_dato(datos)
-            if cantidad==1:
-                mb.showinfo("Informacion!", "Los datos fueron modificados existosamente")
-            else:
-                mb.showerror("Error!", "No existe ningun articulo con ese codigo")
+        self.pagina5=ttk.Frame(self.cuaderno1)
+        self.cuaderno1.add(self.pagina5, text="Modificacar articulo")
+        self.labelframe5=ttk.Labelframe(self.pagina5, text="Articulo")
+        self.labelframe5.grid(column=0, row=0, padx=5, pady=5)
+        self.label5=ttk.Label(self.labelframe5, text="Codigo del articulo")
+        self.label5.grid(column=0, row=0, padx=4, pady=4)
+        self.dato_mod=StringVar()
+        self.entry_mod=ttk.Entry(self.labelframe5, textvariable=self.dato_mod)
+        self.entry_mod.grid(column=1, row=0, padx=4, pady=4)
+        self.label6=ttk.Label(self.labelframe5, text="Descripcion:")
+        self.label6.grid(column=0, row=1, padx=4, pady=4)
+        self.descripcion_mod=StringVar()
+        self.descripcion_mod_entry=ttk.Entry(self.labelframe5, textvariable=self.descripcion_mod)
+        self.descripcion_mod_entry.grid(column=1, row=1, padx=4, pady=4)
+        self.label7=ttk.Label(self.labelframe5, text="Precio:")
+        self.label7.grid(column=0, row=2, padx=4, pady=4)
+        self.precio_mod=tk.StringVar()
+        self.precio_mod_entry=ttk.Entry(self.labelframe5, textvariable=self.precio_mod)
+        self.precio_mod_entry.grid(column=1, row=2, padx=4, pady=4)
+        self.boton2=ttk.Button(self.labelframe5, text="Consultar", command=self.consulta_mod)
+        self.boton2.grid(column=1, row=3, padx=4, pady=4)
+        self.boton3=ttk.Button(self.labelframe5, text="Modificar", command=self.editar)
+        self.boton3.grid(column=1, row=4, padx=4, pady=4)
     
-    
+    def consulta_mod(self):
+        codigo=(self.dato_mod.get(), )
+        respuesta=self.articulo1.consulta_mod(codigo)
 
-aplicacion1=FormularioArticulos()
+        if len(respuesta) > 0:
+            self.descripcion_mod.set(respuesta[0][0])
+            self.precio_mod.set(respuesta[0][1])
+        else:
+            mb.showerror("Error!", "No existe ningun articulo con ese codigo")
+            self.descripcion_mod.set("")
+            self.precio_mod.set("")
+    
+    def editar(self):
+        actualizacion=(self.descripcion_mod.get(), 
+        self.precio_mod.get(),
+        self.dato_mod.get()
+        )
+        respuesta=self.articulo1.modificacion(actualizacion)
+        if respuesta > 0:
+            mb.showinfo("Informacion", "EL articulo ha sido actualizado")
+        else:
+            mb.showerror("Error!", "No existe ningun articulo con ese codigo")
+
+aplicacion=FormularioArticulos()
